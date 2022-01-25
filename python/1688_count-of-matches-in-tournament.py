d9fed43df59837952eec3b7b5e9e94ff7651a19d
caomingpei <caomingpei@outlook.com>
2022-01-25 09:59:58 +0800

    1688_count-of-matches-in-tournament.py

---
+++ python/1688_count-of-matches-in-tournament.py
@@ -0,0 +1,11 @@
+from typing import *
+n=14
+
+class Solution:
+    def numberOfMatches(self, n: int) -> int:
+        return n-1
+
+
+if __name__ == '__main__':
+    res = Solution()
+    print(res.numberOfMatches(n))
